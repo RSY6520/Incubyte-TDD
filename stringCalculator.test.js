9ff86d7015c2fa858450cbd9ffed19bab90c4126
Ramakant Yadav <ext.ramakant@zeliot.in>
2025-10-03 11:31:04 +0530

    test: single number

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,4 +4,9 @@ describe('String Calculator - Incubyte TDD Kata', () => {
   test('returns 0 for empty string', () => {
     expect(add("")).toBe(0);
   });
+
+    test('returns the number for single input', () => {
+    expect(add("1")).toBe(1);
+  });
+
 });

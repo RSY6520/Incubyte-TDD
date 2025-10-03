e1385208f8e6ba90bc2f820f420afbf7ddb69e74
Ramakant Yadav <ext.ramakant@zeliot.in>
2025-10-03 11:35:26 +0530

    refactor: common summing helper

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,9 +1,7 @@
 function add(numbers) {
   if (numbers === "") return 0;
   const parts = numbers.split(",");
-  if (parts.length === 1) return parseInt(parts[0], 10);
-
-  return parseInt(parts[0], 10) + parseInt(parts[1], 10);
+  return parts.map(n => parseInt(n, 10)).reduce((a, b) => a + b, 0);
 }
 
 module.exports = { add };
